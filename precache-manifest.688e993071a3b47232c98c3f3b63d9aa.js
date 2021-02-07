fb5cb69c4b9f6f3da4c3991b2074545fcb849241
ArtemFrolv <papich2648@gmail.com>
2021-02-07 18:37:39 +0300

    Updates

---
+++ precache-manifest.688e993071a3b47232c98c3f3b63d9aa.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47e306565f4178c28eb140842f3c5eef",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "060b51185ce39f7dccba",
+    "url": "/project/static/css/main.93fd8782.chunk.css"
+  },
+  {
+    "revision": "ff284d1f2dcc31f4f573",
+    "url": "/project/static/js/2.111d95fc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project/static/js/2.111d95fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "060b51185ce39f7dccba",
+    "url": "/project/static/js/main.ce412656.chunk.js"
+  },
+  {
+    "revision": "00779be5a75421897f44",
+    "url": "/project/static/js/runtime-main.102d06f0.js"
+  },
+  {
+    "revision": "18c25cafb983d1321228a42b2e8c4be3",
+    "url": "/project/static/media/brown.18c25caf.svg"
+  },
+  {
+    "revision": "772ae131f4531e577501c17b074936b3",
+    "url": "/project/static/media/cow.772ae131.svg"
+  },
+  {
+    "revision": "c4c8a342f33c6f7e1cf384260ba53532",
+    "url": "/project/static/media/creeper.c4c8a342.svg"
+  },
+  {
+    "revision": "85605244d1b72a7129f7764ce7bd4028",
+    "url": "/project/static/media/enderman.85605244.svg"
+  },
+  {
+    "revision": "e91bb9609ea8f03605ec44ac4a115bb1",
+    "url": "/project/static/media/farmer.e91bb960.svg"
+  },
+  {
+    "revision": "f6b65cd66053281fdee219ac1a58ee96",
+    "url": "/project/static/media/main.f6b65cd6.jpg"
+  },
+  {
+    "revision": "c3b390debf6798e942203664e9647ac6",
+    "url": "/project/static/media/recycle.c3b390de.png"
+  },
+  {
+    "revision": "6143437c38f6bc1834f46a07c1b99d63",
+    "url": "/project/static/media/sheep.6143437c.svg"
+  },
+  {
+    "revision": "b9adc7d844e3f70f77195fe51b9e84b3",
+    "url": "/project/static/media/skeleton.b9adc7d8.svg"
+  },
+  {
+    "revision": "fffe12e0e5b1825eeebc333edc6f5d35",
+    "url": "/project/static/media/spider.fffe12e0.svg"
+  },
+  {
+    "revision": "9b0f89f1c794a27b4ea95a2d53db6e77",
+    "url": "/project/static/media/steve.9b0f89f1.svg"
+  },
+  {
+    "revision": "13980704c39efd49e348820d6523ede4",
+    "url": "/project/static/media/white.13980704.svg"
+  },
+  {
+    "revision": "2b4cf4f9ef4bd253e0d3d14f7ad8053d",
+    "url": "/project/static/media/zombie.2b4cf4f9.svg"
+  }
+]);
